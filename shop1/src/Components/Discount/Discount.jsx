185b168574b04d804832cf84a632fcc0d69287dc
NickPavlovskii <84456340+NickPavlovskii@users.noreply.github.com>
2023-02-04 12:25:05 +0300

    ...

--- shop1/src/Components/Discount/Discount.jsx
+++ shop1/src/Components/Discount/Discount.jsx
@@ -1,5 +1,7 @@
 import React from "react"
 import Dcart from "./Dcart"
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
 import { FaCaretRight} from 'react-icons/fa';
 const Discount = () => {
   return (
